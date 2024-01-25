412ea6c0f131ed2283232099e5ffe3e35c8f0d88
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-25 21:22:37 +0100

    feat: build on bash script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+source /opt/ros/humble/setup.bash && source install/setup.bash
+colcon build --continue-on-error --symlink-install
+source /opt/ros/humble/setup.bash && source install/setup.bash
+colcon build --continue-on-error --symlink-install
